bace4179bb4e90fc852114346dabea6694c9a94b
Bart Blommaerts <daggieken@hotmail.com>
2018-05-11 10:47:07 +0200

    generated from start.spring.io

---
+++ src/main/java/be/bbconsulting/postalcalculator/PostalCalculatorApplication.java
@@ -0,0 +1,12 @@
+package be.bbconsulting.postalcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalCalculatorApplication.class, args);
+	}
+}
