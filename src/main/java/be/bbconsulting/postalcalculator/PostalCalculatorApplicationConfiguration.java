1ed4c7ba732a2f9c7bc91afb9ee09f15f7d499fc
Bart Blommaerts <daggieken@hotmail.com>
2018-06-11 22:06:00 +0200

    upgrade to Spring Boot 2.0.2 Important: StreamMessageConverter for overriding MessageConvertors

--- src/main/java/be/bbconsulting/postalcalculator/PostalCalculatorApplicationConfiguration.java
+++ src/main/java/be/bbconsulting/postalcalculator/PostalCalculatorApplicationConfiguration.java
@@ -2,6 +2,7 @@ package be.bbconsulting.postalcalculator;
 
 import be.bbconsulting.postalcalculator.events.EventChannel;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamMessageConverter;
 import org.springframework.cloud.stream.schema.avro.AvroSchemaMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.util.MimeType;
 public class PostalCalculatorApplicationConfiguration {
 
     @Bean
+    @StreamMessageConverter
     public MessageConverter eventMessageConverter() {
         AvroSchemaMessageConverter avroSchemaMessageConverter = new AvroSchemaMessageConverter(MimeType.valueOf("avro/bytes"));
         avroSchemaMessageConverter.setSchemaLocation(new ClassPathResource("avro/Event.avsc"));
